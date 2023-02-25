55d901bd98ccb8d3a7e7e5f120afbef65e3f301a
Lucas-Melo0 <lucassss8v@gmail.com>
2023-02-25 11:18:43 -0300

    feat: initial stucture for beer service layer

---
+++ core/beer/service.go
@@ -0,0 +1,30 @@
+package beer
+
+type UseCase interface {
+	GetAll() ([]*Beer, error)
+	Get(ID int) (*Beer, error)
+	Store(b *Beer) error
+	Update(b *Beer) error
+	Remove(ID int) error
+}
+type Service struct{}
+
+func NewService() *Service {
+	return &Service{}
+}
+func (s *Service) GetAll() ([]*Beer, error) {
+	return nil, nil
+}
+
+func (s *Service) Get(ID int64) (*Beer, error) {
+	return nil, nil
+}
+func (s *Service) Store(b *Beer) error {
+	return nil
+}
+func (s *Service) Update(b *Beer) error {
+	return nil
+}
+func (s *Service) Remove(ID int64) error {
+	return nil
+}
